e33cb823deb94507e14502fa4bd17387261883ed
Mark Graham <mgraham@horizon-asset.co.uk>
2020-09-23 08:52:19 +0100

    add td-encloser and setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='td-encloser',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Package containing the TD-ENCLOSER algorithm',
+    author='Mark Graham',
+    license='MIT License',
+)
